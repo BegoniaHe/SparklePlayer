20c23388f7b73de53d5b400726a1ebfd3db10c69
Tianyi,HE <163421589+BegoniaHe@users.noreply.github.com>
2025-06-20 15:38:56 +0200

    Update SongMessage.java

--- src/com/sparkle/model/SongMessage.java
+++ src/com/sparkle/model/SongMessage.java
@@ -8,14 +8,6 @@ package com.sparkle.model;
 public class SongMessage {
     private static int i = 1000;
 
-    public static final int ADDMUSIC = (i++); // 添加音乐
-    public static final int LOCALDELMUSIC = (i++); // 本地歌曲列表删除歌曲
-    public static final int LOCALADDLIKEMUSIC = (i++);// 本地添加喜欢歌曲
-    public static final int LOCALUNLIKEMUSIC = (i++);// 本地取消喜欢歌曲
-    public static final int LIKEDELMUSIC = (i++);// 喜欢列表删除歌曲
-    public static final int SCANEDMUSIC = (i++); // 扫描音乐
-    public static final int UPDATEMUSIC = (i++); // 更新音乐内容
-
     public static final int INITMUSIC = (i++); // 初始化歌曲操作
     public static final int REINITMUSIC = (i++); // 重新播放歌曲
     public static final int PREMUSIC = (i++); // 上一首歌曲操作
@@ -26,7 +18,6 @@ public class SongMessage {
     public static final int NEXTMUSIC = (i++); // 下一首歌曲操作
     public static final int ERRORMUSIC = (i++); // 歌曲错误操作
     public static final int SEEKTOMUSIC = (i++); // 歌曲快进操作
-    public static final int RANDOMMUSIC = (i++); // 随机选择歌曲操作
 
     public static final int SERVICEPLAYMUSIC = (i++); // 服务播放歌曲操作
     public static final int SERVICEPLAYINIT = (i++); // 服务播放歌曲操作
@@ -38,16 +29,6 @@ public class SongMessage {
     public static final int SERVICESEEKTOMUSIC = (i++); // 服务快进歌曲
     public static final int SERVICEERRORMUSIC = (i++); // 服务播放歌曲错误操作
 
-    public static final int SINGERPHOTOLOADED = (i++); // 歌手写真图片加载完成
-    public static final int ALUBMPHOTOLOADED = (i++); // 歌曲专辑图片加载完成
-    public static final int KSCDOWNLOADED = (i++); // ksc歌词下载完成
-    public static final int KSCLOADED = (i++); // ksc歌词加载完成
-
-    public static final int DESLRCSHOWORHIDE = (i++); // 桌面歌词显示或者隐藏
-    public static final int DESLRCSHOWORHIDEED = (i++); // 桌面歌词显示或者隐藏完成
-    public static final int DESLRCLOCKORUNLOCK = (i++); // 桌面歌词锁定或者解锁
-    public static final int DESLRCLOCKORUNLOCKED = (i++); // 桌面歌词锁定或者解锁完成
-
     public static final int KSCTYPELRC = (i++); // lrcview ksc歌词
     public static final int LRCKSCDOWNLOADED = (i++); // lrcview ksc歌词下载完成
     public static final int LRCKSCLOADED = (i++); // lrcview ksc歌词下载完成
@@ -57,7 +38,6 @@ public class SongMessage {
     public static final int DESKSCLOADED = (i++); // 桌面 ksc歌词下载完成
 
     public static final int KSCTYPELOCK = (i++); // 锁屏ksc歌词
-    public static final int LOCKKSCDOWNLOADED = (i++); // 锁屏 ksc歌词下载完成
     public static final int LOCKKSCLOADED = (i++); // 锁屏 ksc歌词下载完成
 
     /**
